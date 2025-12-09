62c9feaba3d2c10912119cac3a632fda592d3fc7
Jackson <hyo961015@naver.com>
2025-12-09 10:29:03 +0900

    Further fixes to #1654: `@JsonDeserialize(contentUsing)` ignored when `@JsonTypeInfo(use = Id.NONE)` (#5479)

--- src/test/java/tools/jackson/databind/jsontype/NoTypeInfo1654Test.java
+++ src/test/java/tools/jackson/databind/jsontype/NoTypeInfo1654Test.java
@@ -6,9 +6,13 @@ import org.junit.jupiter.api.Test;
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
+import tools.jackson.core.JacksonException;
+import tools.jackson.core.JsonGenerator;
 import tools.jackson.core.JsonParser;
+
 import tools.jackson.databind.*;
 import tools.jackson.databind.annotation.JsonDeserialize;
+import tools.jackson.databind.annotation.JsonSerialize;
 import tools.jackson.databind.testutil.DatabindTestUtil;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -37,28 +41,63 @@ class NoTypeInfo1654Test extends DatabindTestUtil
     }
 
     static class Value1654UntypedContainer {
-        //@JsonDeserialize(contentUsing = Value1654Deserializer.class)
         @JsonTypeInfo(use = JsonTypeInfo.Id.NONE)
         public List<Value1654> values;
 
-        protected Value1654UntypedContainer() {
-        }
+        protected Value1654UntypedContainer() { }
 
         public Value1654UntypedContainer(Value1654... v) {
             values = Arrays.asList(v);
         }
     }
 
-    /*
+    static class Value1654UsingCustomSerDeserUntypedContainer {
+        @JsonDeserialize(contentUsing = Value1654Deserializer.class)
+        @JsonSerialize(contentUsing = Value1654Serializer.class)
+        @JsonTypeInfo(use = JsonTypeInfo.Id.NONE)
+        public List<Value1654> values;
+
+        protected Value1654UsingCustomSerDeserUntypedContainer() { }
+
+        public Value1654UsingCustomSerDeserUntypedContainer(Value1654... v) {
+            values = Arrays.asList(v);
+        }
+    }
+
+    static class SingleValue1654UsingCustomSerDeserUntyped {
+        @JsonDeserialize(using = Value1654Deserializer.class)
+        @JsonSerialize(using = Value1654Serializer.class)
+        @JsonTypeInfo(use = JsonTypeInfo.Id.NONE)
+        public Value1654 value;
+
+        protected SingleValue1654UsingCustomSerDeserUntyped() { }
+
+        public SingleValue1654UsingCustomSerDeserUntyped(Value1654 v) {
+            value = v;
+        }
+    }
+
     static class Value1654Deserializer extends ValueDeserializer<Value1654> {
         @Override
         public Value1654 deserialize(JsonParser p, DeserializationContext ctxt) {
-            //JsonNode n = ctxt.readTree(p);
-            p.skipChildren();
-            return new Value1654(13);
+            JsonNode n = ctxt.readTree(p);
+            if (!n.has("v")) {
+                ctxt.reportInputMismatch(Value1654.class, "Bad JSON input (no 'v'): " + n);
+            }
+            return new Value1654(n.path("v").intValue());
         }
     }
-    */
+
+
+    static class Value1654Serializer extends ValueSerializer<Value1654> {
+        @Override
+        public void serialize(Value1654 value, JsonGenerator gen, SerializationContext ctxt)
+                throws JacksonException {
+            gen.writeStartObject(value);
+            gen.writeNumberProperty("v", value.x);
+            gen.writeEndObject();
+        }
+    }
 
     private final ObjectMapper MAPPER = newJsonMapper();
 
@@ -79,9 +118,9 @@ class NoTypeInfo1654Test extends DatabindTestUtil
         assertEquals(2, result.values.get(1).x);
     }
 
-    // [databind#1654]: override, no polymorphic type id
+    // [databind#1654]: override, no polymorphic type id, serialization
     @Test
-    void withNoTypeInfoOverrideSer() throws Exception {
+    void withNoTypeInfoDefaultSer() throws Exception {
         Value1654UntypedContainer cont = new Value1654UntypedContainer(
                 new Value1654(3),
                 new Value1654(7)
@@ -90,15 +129,59 @@ class NoTypeInfo1654Test extends DatabindTestUtil
                 MAPPER.writeValueAsString(cont));
     }
 
-    // [databind#1654]
+    // [databind#1654]: override, no polymorphic type id, deserialization
     @Test
-    void withNoTypeInfoOverrideDeser() throws Exception {
-        // and then actual failing case
+    void withNoTypeInfoDefaultDeser() throws Exception {
         final String noTypeJson = a2q(
                 "{'values':[{'x':3},{'x':7}]}"
         );
-        Value1654UntypedContainer unResult = MAPPER.readValue(noTypeJson, Value1654UntypedContainer.class);
+        Value1654UntypedContainer unResult = MAPPER.readValue(noTypeJson,
+                Value1654UntypedContainer.class);
         assertEquals(2, unResult.values.size());
         assertEquals(7, unResult.values.get(1).x);
+    }
+
+    // [databind#1654]: override, no polymorphic type id, custom serialization
+    @Test
+    void withNoTypeInfoOverrideSer() throws Exception {
+        Value1654UsingCustomSerDeserUntypedContainer cont = new Value1654UsingCustomSerDeserUntypedContainer(
+                new Value1654(1),
+                new Value1654(2)
+        );
+        assertEquals(a2q("{'values':[{'v':1},{'v':2}]}"),
+                MAPPER.writeValueAsString(cont));
+    }
+
+    // [databind#1654]: override, no polymorphic type id, custom deserialization
+    @Test
+    void withNoTypeInfoOverrideDeser() throws Exception {
+        final String noTypeJson = a2q(
+                "{'values':[{'v':3},{'v':7}]}"
+        );
+        Value1654UsingCustomSerDeserUntypedContainer unResult = MAPPER.readValue(noTypeJson,
+                Value1654UsingCustomSerDeserUntypedContainer.class);
+        assertEquals(2, unResult.values.size());
+        assertEquals(3, unResult.values.get(0).x);
+        assertEquals(7, unResult.values.get(1).x);
+    }
+
+    // // And then validation for individual value, not in Container
+
+    // override, no polymorphic type id, custom serialization
+    @Test
+    void singleWithNoTypeInfoOverrideSer() throws Exception {
+        SingleValue1654UsingCustomSerDeserUntyped wrapper = new SingleValue1654UsingCustomSerDeserUntyped(
+                new Value1654(42));
+        assertEquals(a2q("{'value':{'v':42}}"),
+                MAPPER.writeValueAsString(wrapper));
+    }
+
+    // override, no polymorphic type id, custom deserialization
+    @Test
+    void singleWithNoTypeInfoOverrideDeser() throws Exception {
+        String noTypeJson = a2q("{'value':{'v':42}}");
+        SingleValue1654UsingCustomSerDeserUntyped result = MAPPER.readValue(noTypeJson,
+                SingleValue1654UsingCustomSerDeserUntyped.class);
+        assertEquals(42,result.value.x);
     }
 }
